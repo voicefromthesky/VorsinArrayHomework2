5250c064a8c4ddd211b6a8a8a5b21f57c08d3425
voicefromthesky <andrewdoverin@gmail.com>
2021-10-11 12:45:36 +0300

    first commit

---
+++ VorsinArrayHomework2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VorsinArrayHomework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = { 1, 2, 3, 4 };
+            Console.WriteLine("Which values would you like yto add?");
+            string inputNumbers = Console.ReadLine();
+            int[] secondArray = Array.ConvertAll(inputNumbers.Split(' '), int.Parse);
+            Array.Resize(ref firstArray, firstArray.Length + secondArray.Length);
+            Array.Copy(secondArray, 0, firstArray, firstArray.Length - secondArray.Length, secondArray.Length);
+
+            Console.WriteLine(String.Join(",", firstArray));
+
+
+        }
+    }
+}
